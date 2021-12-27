9654ba8f5faaddcd076bdaafafb7f4506d1fc3a1
卢文杰 <luwenjie@bwton.com>
2021-12-27 16:43:45 +0800

    hello world

---
+++ src/com/wenjie/leetcode/hot100/Hello.java
@@ -0,0 +1,14 @@
+package com.wenjie.leetcode.hot100;
+
+/**
+ * Description:  hello
+ * Created on 2021/12/27 16:42
+ * @author iswenjie.lu
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
