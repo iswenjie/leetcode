74952606ea44f653f936ba61696b1ca52c2e0d86
iswenjie <iswenjie.lu@gmail.com>
2022-01-12 17:34:58 +0800

    寻找K大优化

--- src/com/wenjie/leetcode/hot100/mid/FindKthLargest.java
+++ src/com/wenjie/leetcode/hot100/mid/FindKthLargest.java
@@ -1,7 +1,5 @@
 package com.wenjie.leetcode.hot100.mid;
 
-import java.util.Arrays;
-
 /**
  * Description:  215. 数组中的第K个最大元素
  * tag: 排序
@@ -14,25 +12,50 @@ public class FindKthLargest {
     public static void main(String[] args) {
         FindKthLargest v = new FindKthLargest();
 
-        int[] arr = {4, 3, 2, 7, 8, 3, 2, 1};
+        int[] arr = {7, 6, 5, 4, 3, 2, 1};
         v.findKthLargest(arr, 2);
     }
 
     public int findKthLargest(int[] nums, int k) {
-        mergeSort(nums, 0, nums.length - 1);
+        int i = quickSelect(nums, 0, nums.length - 1, k);
         return nums[nums.length - k];
     }
 
     /**
      * 快速选择
-     * 在快熟排序中判断第k大在哪个区间，丢弃另外一半区间继续往下（k可能需要截取）
-     * TODO
+     * 在快熟排序中判断第k大在哪个区间，丢弃另外一半区间继续往下
      * @param nums
      * @param start
      * @param end
      */
     private int quickSelect(int[] nums, int start, int end, int k) {
-        return 0;
+        if (start == end) {
+            return nums[start];
+        }
+        int left = start, right = end;
+        int val = nums[start];
+        while (left <= right) {
+            while (left <= right && nums[left] < val) {
+                left++;
+            }
+            while (left <= right && nums[right] > val) {
+                right--;
+            }
+            if (left <= right) {
+                swap(nums, left, right);
+                left++;
+                right--;
+            }
+        }
+        int rightCount = end - left + 1;
+        if (rightCount >= k) {
+            // k在右边=
+            return quickSelect(nums, left, end, k);
+        } else {
+            // k在左边
+            k = k - rightCount;
+            return quickSelect(nums, start, right, k);
+        }
     }
 
 
@@ -103,7 +126,6 @@ public class FindKthLargest {
         int tmp = nums[idx1];
         nums[idx1] = nums[idx2];
         nums[idx2] = tmp;
-        System.out.println(Arrays.toString(nums));
     }
 
 
--- src/com/wenjie/leetcode/sort/QuickSort.java
+++ src/com/wenjie/leetcode/sort/QuickSort.java
@@ -11,7 +11,7 @@ public class QuickSort implements Sort {
     public static void main(String[] args) {
         Sort v = new QuickSort();
 
-        int[] arr = {4, 3, 2, 7, 8, 3, 2, 1};
+        int[] arr = {1,4, 3, 2, 7, 8, 3, 2};
         v.sortArray(arr);
     }
 
@@ -27,7 +27,7 @@ public class QuickSort implements Sort {
         }
         int left = start, right = end;
         // 获取一个值用来比较，有随机的，这里简单直接使用中间坐标
-        int pivot = nums[(left + right) >> 1];
+        int pivot = nums[start];
         while (left <= right) {
             // 找到左边比pivot大的数
             while (left <= right && nums[left] < pivot) {
